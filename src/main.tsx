32f026027a53041bc45c7faf16cb22b405359f85
TadashiYamazaki <yama3ki.tada4@gmail.com>
2019-10-16 06:20:19 +0900

    1st commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <React.Fragment>
+        <h1>Hello, world!</h1>
+    </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
